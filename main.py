a16748a76f346d340c5b97264c878d61c291c76b
tmbrlkV <dn00p@protonmail.com>
2018-12-29 19:16:07 +0300

    feature/path
    * Added support for custom path for pictures;

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import sys
-import concurrent.futures
+import os
+from threading import Thread
 
 from scrapper import Scrapper
 
 def scrap_pictures(scrapper):
     while True:
-        scrapper.generate_random_url()
-        scrapper.scrape()
+        img = scrapper.generate_random_url()
+        scrapper.scrape(img)
 
-path = sys.argv[1] if len(sys.argv) > 1 else None
+if __name__ == '__main__':
+    path = sys.argv[1] if len(sys.argv) > 1 else None
 
-picture_scrapper = Scrapper(path)
+    if not os.path.exists(path):
+        os.makedirs(path)
 
-try:
-    with concurrent.futures.ThreadPoolExecutor(1) as executor:
-        executor.submit(scrap_pictures, picture_scrapper)
-except:
-    sys.exit(-1)
+    picture_scrapper = Scrapper(path)
+    threads = []
 
+    try:
+        thread = Thread(target = scrap_pictures, args = (picture_scrapper,))
+        thread.start()
+        threads.append(thread)
+    except KeyboardInterrupt:
+        for t in threads:
+            t.join(1)
--- scrapper/__init__.py
+++ scrapper/__init__.py
@@ -5,11 +5,10 @@ import urllib.error
 import urllib.request
 
 class Scrapper:
-    img = { }
     # need to define empty sizes if imgur doesn't have the image
     empty_file_sizes = [0, 503, 4939, 4940, 4941, 12003, 5556]
     url = 'https://i.imgur.com'
-    path = '.'
+    path = './images'
 
     def __init__(self, path):
         if path is not None:
@@ -22,25 +21,23 @@ class Scrapper:
         seed = string.ascii_uppercase + string.digits + string.ascii_lowercase
         image_id = ''.join((random.choice(seed) for _ in range(amount)))
 
-        self.img = { 'url': f'{self.url}/{image_id}.jpg', 'name': f'{image_id}.jpg' }
+        return { 'url': f'{self.url}/{image_id}.jpg', 'name': f'{image_id}.jpg' }
 
-    def scrape(self):
+    def scrape(self, img):
         try:
-            filename = f'''{self.path}/{self.img['name']}'''
-            urllib.request.urlretrieve(self.img['url'], filename)
-            file = os.path.getsize(self.img['name'])
+            filename = f'''{self.path}/{img['name']}'''
+            urllib.request.urlretrieve(img['url'], filename)
+            file = os.path.getsize(filename)
 
             if file in self.empty_file_sizes:
-                print('Invalid image url')
+                print('[-] Invalid image url')
                 os.remove(filename)
                 return 404
             else:
-                print(f'Valid image url {self.img}')
+                print(f'''[+] Valid image url {img['url']}''')
         except urllib.error.HTTPError:
             return 404
         except urllib.error.ContentTooShortError:
             return 400
-        finally:
-            self.img = { }
 
         return 200
